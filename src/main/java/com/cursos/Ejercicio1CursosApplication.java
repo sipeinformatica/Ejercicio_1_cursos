5944e7481a485545e2713e7ad8efb545c8c97d3a
Héctor Esteve <hector.esteve@sipeinformatica.com>
2022-05-21 17:38:18 +0200

    commit inicial

---
+++ src/main/java/com/cursos/Ejercicio1CursosApplication.java
@@ -0,0 +1,13 @@
+package com.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1CursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1CursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursos/Ejercicio1CursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1CursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
